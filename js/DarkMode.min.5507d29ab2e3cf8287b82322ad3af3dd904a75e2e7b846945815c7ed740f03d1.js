4c69aad97913f3bbed97e7cfc9046263739be9d6
nbelzer <nbelzer@users.noreply.github.com>
2020-03-09 13:11:10 +0000

    deploy: 3752d771069ca9fe77da09ea9d2fad2f3a267bac

---
+++ js/DarkMode.min.5507d29ab2e3cf8287b82322ad3af3dd904a75e2e7b846945815c7ed740f03d1.js
@@ -0,0 +1,5 @@
+const darkClass='dark-mode';const defaultTheme='light';const container=document.body;let currentTheme=defaultTheme;function enableDarkMode(){container.classList.add(darkClass);currentTheme='dark';}
+function enableLightMode(){container.classList.remove(darkClass);currentTheme='light';}
+function toggle(){if(currentTheme==='light'){enableDarkMode();}else if(currentTheme==='dark'){enableLightMode()}}
+const colorSchemeDarkMatch=window.matchMedia('(prefers-color-scheme: dark)');const colorSchemeLightMatch=window.matchMedia('(prefers-color-scheme: light)');if(colorSchemeDarkMatch.matches){enableDarkMode();}else if(colorSchemeLightMatch.matches){enableLightMode()}
+colorSchemeDarkMatch.addListener(e=>{if(e.matches)enableDarkMode();});colorSchemeLightMatch.addListener(e=>{if(e.matches)enableLightMode();});
